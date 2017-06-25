38b670ffa753ee2d1700e945e8c6f215a86ce2f4
Blo <Blo@Blo-PC>
2017-06-25 21:03:06 +0300

    Homework, a simple calculator tried with OOP Laita Claudiu

--- JavaHomeworkLaitaClaudiu/src/homeworkcalculatoroop/Button.java
+++ JavaHomeworkLaitaClaudiu/src/homeworkcalculatoroop/Button.java
@@ -3,41 +3,40 @@ package homeworkcalculatoroop;
 import java.util.Scanner;
 
 /**
- * My first Java program.
- * Calculator
- * VERSION 0.0.0.0.0.2
+ * My first Java program. Calculator VERSION 0.0.0.0.0.2
+ *
  * @author Claudiu Laita
  */
-public class Button extends Screen{
-    
+public class Button extends Screen {
+
     private static double num1;
     private static double num2;
     private static String sum;
     public static Scanner inp = new Scanner(System.in);
 
-    public static void compute(){
-        
+    public static void compute() {
+
         firstNumber();
         operator();
         secondNumber();
         eqsOperator();
-        
-        
+
     }
+
     // Allows the input of the first number and checks if it is valid;
     public static void firstNumber() {
         askFirst();
         if (inp.hasNextDouble()) {
             num1 = inp.nextDouble();
-        }else if(inp.next().equals("clear")){
+        } else if (inp.next().equals("clear")) {
             clear();
-        }else{
+        } else {
             displayErr1();
         }
     }
-    
+
     //Allows the input of the operator 
-    public static void operator(){
+    public static void operator() {
         askOperator();
         sum = inp.next();
         switch (sum) {
@@ -54,45 +53,45 @@ public class Button extends Screen{
                 break;
         }
     }
-    
+
     //Allows the input of the second number and checks if it is valid;
     public static void secondNumber() {
         askSecond();
         if (inp.hasNextDouble()) {
             num2 = inp.nextDouble();
-        }else if(inp.next().equals("clear")){
+        } else if (inp.next().equals("clear")) {
             clear();
-        }else{
+        } else {
             displayErr1();
         }
     }
-    
+
     /*Allows the input of the "=" operator and checks if it is valid,
-        and also shows the result if everything is ok;*/  
-    public static void eqsOperator(){
+        and also shows the result if everything is ok;*/
+    public static void eqsOperator() {
         askEquals();
-        if(inp.next().equals("clear")){
+        if (inp.next().equals("clear")) {
             clear();
-        }else if(null == sum){
+        } else if (null == sum) {
             displayErr1();
-        }else switch (sum) {
-            case "+":
-                display = num1 + num2;
-                break;
-            case "-":
-                display = num1 - num2;
-                break;
-            case "*":
-                display = num1 * num2;
-                break;
-            case "/":
-                display = num1 / num2;
-                break;
-            default:
-                displayErr1();
-                break;
-        } 
+        } else {
+            switch (sum) {
+                case "+":
+                    display = num1 + num2;
+                    break;
+                case "-":
+                    display = num1 - num2;
+                    break;
+                case "*":
+                    display = num1 * num2;
+                    break;
+                case "/":
+                    display = num1 / num2;
+                    break;
+                default:
+                    displayErr1();
+                    break;
+            }
+        }
     }
 }
-    
-    
--- JavaHomeworkLaitaClaudiu/src/homeworkcalculatoroop/Screen.java
+++ JavaHomeworkLaitaClaudiu/src/homeworkcalculatoroop/Screen.java
@@ -3,45 +3,44 @@ package homeworkcalculatoroop;
 import static homeworkcalculatoroop.Button.compute;
 
 /**
- * My first Java program.
- * Calculator
- * VERSION 0.0.0.0.0.2
+ * My first Java program. Calculator VERSION 0.0.0.0.0.2
+ *
  * @author Claudiu Laita
  */
 public class Screen {
-    
+
     public static double display = 0;
-    
+
     //Start up message
-    public static void display(){
+    public static void display() {
         System.out.println("\n" + "This is a calculator. "
-                + "Use 'clear' comand to retry the operation" + "\n" + display);
+                + "Use 'clear' command to retry the operation" + "\n" + display);
     }
-    
-    public static void askFirst(){
+
+    public static void askFirst() {
         System.out.println("\n" + "Please introduce your first number:");
     }
-    
-    public static void askSecond(){
+
+    public static void askSecond() {
         System.out.println("Please introduce your second number:");
     }
-    
-    public static void askOperator(){
+
+    public static void askOperator() {
         System.out.println("Please introduce operator(+/-/*//)");
     }
-    
-    public static void askEquals(){
+
+    public static void askEquals() {
         System.out.println("Please introduce = to show result");
     }
-    
+
     //Should restart the calculator
-    public static void clear(){   
-        System.out.print("\033[H\033[2J"); 
+    public static void clear() {
+        System.out.print("\033[H\033[2J");
         System.out.flush();
         compute();
     }
-    
-    public static void displayErr1(){
+
+    public static void displayErr1() {
         System.out.println("Error, you introduced wrong data. I quit.");
         System.exit(0);
     }
